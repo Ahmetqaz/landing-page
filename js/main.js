bc346a3bdac3b112515ecc03e49e6eee10292302
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2023-06-17 20:20:34 +0500

    responsive

--- js/main.js
+++ js/main.js
@@ -29,46 +29,37 @@ if (header)
     }
   });
 
-///
-///
-/// slick slider
 
-///
-///
-/// tabEvents
-const toggleBody = (isClosed) => {
-  if (isClosed) {
-    document.body.classList.add("active");
-    if (menu) closeMenu();
-  } else {
-    document.body.classList.remove("active");
-  }
-};
-document.querySelectorAll(`[data-event="tabEvent"]`).forEach((eventBtn) => {
-  const tab = document.querySelector(eventBtn.getAttribute("data-tab"));
-  if (tab) {
-    eventBtn.onclick = (e) => {
-      e.preventDefault();
-      tab.classList.toggle("active");
-      toggleBody(tab.classList.contains("active"));
-    };
-    tab.onclick = (e) => {
-      if (e.target === e.currentTarget) {
-        tab.classList.toggle("active");
-        toggleBody(tab.classList.contains("active"));
+/// Tab
+const tabBtn = document.querySelectorAll(".tabBtn");
+if(tabBtn){
+  const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      console.log("now active");
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
       }
-    };
-  }
-});
-document.querySelectorAll(`[data-toggle]`).forEach((toggleBtn) => {
-  console.log("btn ->");
-  toggleBtn.onclick = () =>
-    toggleBtn.classList.toggle(toggleBtn.getAttribute("data-toggle"));
-});
-///
-///
-///
-///WOW JS
-// new WOW().init({
-//   boxClass: "wow",
-// });
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+}
